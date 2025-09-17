5db75a4cad9347e308b00282634cabf26b397891
Solune Petit <210225@site.asty-moulin.be>
2025-09-17 09:36:51 +0200

    2025-09-17_09h36
    prise de contacte XAML

---
+++ WPF/25-26/Prise de contact XAML/ex1-2/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Prise_de_contact_XAML
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
