0fb94806043a8fc2b64025971edb15827f73aa5a
Solune Petit <210225@site.asty-moulin.be>
2025-09-22 10:51:52 +0200

    2025-09-22_10h51

---
+++ WPF/25-26/UML_chien/PetitSolune_ExerciceChien/Chien.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetitSolune_ExerciceChien
+{
+    internal class Chien
+    {
+        //attributs
+        private string _nom, _race, _pelage, _tag;
+        private double _age, _taille, _poids, _sociabilite;
+        private bool _agresif;
+
+        public Chien(string nom, string race, string pelage, string tag, double age, double taille, double poids, double sociabilite, bool agresif)
+        {
+            _nom = nom;
+            _race = race;
+            _pelage = pelage;
+            _tag = tag;
+            _age = age;
+            _taille = taille;
+            _poids = poids;
+            _sociabilite = sociabilite;
+            _agresif = agresif;
+        }
+
+        public string Sauter()
+        {
+            return $"{_nom} saute de joie !";
+        }
+
+        public string Aboyer()
+        {
+            return $"{_nom} aboie ! Wouaf Wouaf !";
+        }
+
+        public string Manger()
+        {
+            return $"{_nom} mange sa nourriture.";
+        }
+    }
+}
---
+++ WPF/25-26/UML_chien/PetitSolune_ExerciceChien/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace PetitSolune_ExerciceChien
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Chien monChien = new Chien("Rex", "Berger Allemand", "Court", "12345", 5, 60, 30, 8, false);
+
+            Console.WriteLine(monChien.Sauter());
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine(monChien.Aboyer());
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine(monChien.Manger());
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
