ef88240762cb553bcd27e6e9659dc6954b60e4d8
Solune Petit <210225@site.asty-moulin.be>
2025-11-05 09:55:17 +0100

    2025-11-05_09h54
    début ex2 de interfaces dynamiques

---
+++ WPF/25-26/Creation_dynamique/ex2/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ex2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //definition des colonnes
+            ColumnDefinition[] colDef = new ColumnDefinition[4];
+            for (int i = 0; i < 4; i++)
+            {
+                colDef[i] = new ColumnDefinition();
+                grdMain.ColumnDefinitions.Add(colDef[i]);
+            }
+
+            //definition des lignes
+            RowDefinition[] rowDef = new RowDefinition[4];
+            for (int i = 0; i < 4; i++)
+            {
+                rowDef[i] = new RowDefinition();
+                grdMain.RowDefinitions.Add(rowDef[i]);
+            }
+
+            //ajout des boutons
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Button btn = new Button();
+                    btn.Content = $"?";
+                    Grid.SetRow(btn, i);
+                    Grid.SetColumn(btn, j);
+                    grdMain.Children.Add(btn);
+                }
+            }
+        }
+    }
+}
