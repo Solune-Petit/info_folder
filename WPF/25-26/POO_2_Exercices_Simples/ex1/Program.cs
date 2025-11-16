6b64465580d6ab6024026cbfbe719eea0213d39d
Solune-Petit <210225@site.asty-moulin.be>
2025-11-16 20:34:18 +0100

    2025-11-16_20h33
    exrcices simples 2 exercice 1

---
+++ WPF/25-26/POO_2_Exercices_Simples/ex1/Program.cs
@@ -0,0 +1,131 @@
+﻿namespace ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //création d'un tableau 2D pour stocker les informations des voitures
+            string[,] voitures = new string[5, 4]
+            {
+                { "Model S", "Tesla", "ABC123", "100" },
+                { "Civic", "Honda", "DEF456", "50" },
+                { "Corolla", "Toyota", "GHI789", "55" },
+                { "Mustang", "Ford", "JKL012", "60" },
+                { "Impreza", "Subaru", "MNO345", "45" }
+            };
+
+            Vehicule[] garage = new Vehicule[5];
+            //stockage des véhicules dans la classe Vehicule
+            for (int i = 0; i < voitures.GetLength(0); i++)
+            {
+                string modele = voitures[i, 0];
+                string marque = voitures[i, 1];
+                string plaque = voitures[i, 2];
+                uint capaciteMaxReservoir = uint.Parse(voitures[i, 3]);
+                garage[i] = new Vehicule(modele, marque, plaque, capaciteMaxReservoir);
+            }
+
+
+            bool recommancer = false;
+            //boucle principale
+            do
+            {
+                Console.Clear();
+                //affichage des véhicules de la classe Vehicule avec la méthode TypeVehicule
+                foreach (var vehicule in garage)
+                {
+                    Console.WriteLine($"{vehicule.TypeVehicule()}\n");
+                }
+
+                ConsoleKeyInfo key;
+                bool plaqueValide = false;
+                //demander à l'utilisateur si il veut ajouter du carburant à une voiture ou de faire le plein
+                do
+                {
+
+                    //demander l'operation à effectuer
+                    do
+                    {
+                        Console.WriteLine("Voulez-vous :" +
+                            "\n1. Ajouter du carburant à une voiture ?" +
+                            "\n2. Faire le plein à une voiture ?");
+                        key = Console.ReadKey();
+                    } while (key.KeyChar != '1' && key.KeyChar != '2');
+
+                    //demander la plaque de la voiture
+                    Console.WriteLine("\nEntrez la plaque de la voiture :");
+                    string plaqueRecherchee = Console.ReadLine();
+
+                    //rechercher la voiture dans le garage
+                    Vehicule vehiculeTrouve = null;
+                    foreach (var vehicule in garage)
+                    {
+                        if (vehicule.Plaque.Equals(plaqueRecherchee, StringComparison.OrdinalIgnoreCase))
+                        {
+                            vehiculeTrouve = vehicule;
+                            break;
+                        }
+                    }
+
+                    if (vehiculeTrouve != null)
+                    {
+                        //si la voiture est trouvée
+
+                        int temp;
+                        plaqueValide = true;
+
+                        //vérifier si la voiture à déjà le plein ou si l'ajout de carburant dépasse la capacité maximale
+                        if (vehiculeTrouve.CapaciteMaxReservoir == vehiculeTrouve.JaugeCarburant)
+                        {
+                            Console.WriteLine("\nLa voiture a déjà le plein.\n");
+                        }
+                        else if (key.KeyChar == '1')
+                        {
+                            Console.WriteLine("Entrez la quantité de carburant à ajouter :");
+                            uint quantiteAAjouter;
+                            while (!uint.TryParse(Console.ReadLine(), out quantiteAAjouter))
+                            {
+                                Console.WriteLine("Veuillez entrer un nombre valide.");
+                            }
+
+                            //vérifier si l'ajout de carburant dépasse la capacité maximale
+                            temp = (int)(vehiculeTrouve.JaugeCarburant + quantiteAAjouter);
+                            if (temp > vehiculeTrouve.CapaciteMaxReservoir)
+                            {
+                                Console.WriteLine("\nL'ajout de carburant dépasse la capacité maximale du réservoir.\n");
+                            }
+                            else
+                            {
+                                vehiculeTrouve.AjouterCarburant(quantiteAAjouter);
+                            }
+                        }
+                        else if (key.KeyChar == '2')
+                        {
+                            vehiculeTrouve.FaireLePlein();
+                        }
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nVoiture non trouvée.");
+                        }
+                    } while (!plaqueValide) ;
+
+                    //demander à l'utilisateur s'il veut recommencer
+                    char recommanceKey;
+                    do
+                    {
+                        Console.WriteLine("Voulez-vous recommencer ? (o/n)");
+                        recommanceKey = Console.ReadKey().KeyChar;
+                        if (recommanceKey == 'o' || recommanceKey == 'O')
+                        {
+                            recommancer = true;
+                        }
+                        else
+                        {
+                            recommancer = false;
+                        }
+                    } while (recommanceKey != 'o' && recommanceKey != 'O' && recommanceKey != 'n' && recommanceKey != 'N');
+            } while (recommancer);
+        }
+    }
+}
---
+++ WPF/25-26/POO_2_Exercices_Simples/ex1/Vehicule.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ex1
+{
+    internal class Vehicule
+    {
+
+		private uint _jaugeCarburant;
+
+		public uint JaugeCarburant
+		{
+			get { return _jaugeCarburant; }
+			set { _jaugeCarburant = value; }
+		}
+
+		private uint _capaciteMaxReservoir;
+		public uint CapaciteMaxReservoir
+		{
+			get { return _capaciteMaxReservoir; }
+			set { _capaciteMaxReservoir = value; }
+		}
+
+		private string _modele;
+		public string Modele
+		{
+			get { return _modele; }
+		}
+
+		private string _marque;
+		public string Marque
+		{
+			get { return _marque; }
+		}
+
+		private string _plaque;
+		public string Plaque
+		{
+			get { return _plaque; }
+		}
+
+
+		public Vehicule(string modele, string marque, string plaque, uint capaciteMaxReservoir)
+		{
+			_modele = modele;
+			_marque = marque;
+			_plaque = plaque;
+			_capaciteMaxReservoir = capaciteMaxReservoir;
+			_jaugeCarburant = 0;
+        }
+
+
+		public string TypeVehicule()
+		{
+			return $"Modèle: {_modele}, Marque: {_marque}, Plaque: {_plaque}, Capacité du réservoir: {_capaciteMaxReservoir}L, Jauge de carburant: {_jaugeCarburant}L";
+        }
+
+		public void AjouterCarburant(uint quantite)
+		{
+			_jaugeCarburant += quantite;
+        }
+
+		public void FaireLePlein()
+		{
+			_jaugeCarburant = _capaciteMaxReservoir;
+        }
+
+
+    }
+}
