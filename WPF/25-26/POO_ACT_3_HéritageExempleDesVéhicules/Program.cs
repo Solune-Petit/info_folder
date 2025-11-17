208ceea67bbe4a8fff7f6ad927b1be565b9ec8c2
Solune Petit <210225@site.asty-moulin.be>
2025-11-17 08:42:12 +0100

    2025-11-17_08h41
    création exercice POO ACT 3 Héritage Exemple des véhicules

---
+++ WPF/25-26/POO_ACT_3_HéritageExempleDesVéhicules/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace POO_ACT_3_HéritageExempleDesVéhicules
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
