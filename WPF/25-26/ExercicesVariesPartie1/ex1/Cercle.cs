4a91c389ecb4955ec22959eb4aedc90b498a333b
Solune Petit <210225@site.asty-moulin.be>
2025-11-17 10:02:47 +0100

    2025-11-17_10h00
    début ACT POO : EXERCICES Variés Partie 1 ex1

---
+++ WPF/25-26/ExercicesVariesPartie1/ex1/Cercle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex1
+{
+    internal class Cercle
+    {
+		private decimal _rayon;
+		public decimal Rayon
+		{
+			get { return _rayon; }
+			set { _rayon = value; }
+		}
+
+		public Cercle(decimal rayon)
+		{
+			_rayon = rayon;
+        }
+
+		public decimal CalculerAire()
+		{
+            //calculer l'aire du cercle
+			return (decimal)(Math.PI * Math.Pow((double)_rayon, 2));
+        }
+
+		public decimal CalculerPerimetre()
+		{
+            //calculer le périmètre du cercle
+			return (decimal)(2 * Math.PI * (double)_rayon);
+        }
+
+		public string AfficherDetails()
+		{
+			return $"Cercle de rayon {_rayon}: Aire = {CalculerAire()}, Périmètre = {CalculerPerimetre()}";
+        }
+
+    }
+}
---
+++ WPF/25-26/ExercicesVariesPartie1/ex1/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool restart = true;
+            do
+            {
+                string input;
+                do
+                {
+                    Console.WriteLine("Entrez le rayon du cercle (ou tapez 'exit' pour quitter) :");
+                    input = Console.ReadLine();
+                    if (input != null && input.ToLower() != "exit")
+                    {
+                        if (decimal.TryParse(input, out decimal rayon))
+                        {
+                            Cercle cercle = new Cercle(rayon);
+                            Console.WriteLine(cercle.AfficherDetails());
+                        }
+                        else
+                        {
+                            Console.WriteLine("Veuillez entrer un nombre valide pour le rayon.");
+                        }
+                    }
+                } while (input != null && input.ToLower() != "exit");
+
+                Console.WriteLine("Voulez-vous calculer un autre cercle ? (o/n) :");
+                string response = Console.ReadLine();
+                if (response == null || response.ToLower() != "o")
+                {
+                    restart = false;
+                }
+            } while (restart);
+
+
+        }
+    }
+}
