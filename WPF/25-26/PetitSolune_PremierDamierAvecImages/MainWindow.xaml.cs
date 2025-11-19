db22b56769d7a29dfb7d5bfb1293d133df500ad9
Solune Petit <210225@site.asty-moulin.be>
2025-11-19 09:56:50 +0100

    2025-11-19_09h55
    fin exercice 1 echequier

--- WPF/25-26/PetitSolune_PremierDamierAvecImages/MainWindow.xaml.cs
+++ WPF/25-26/PetitSolune_PremierDamierAvecImages/MainWindow.xaml.cs
@@ -19,6 +19,61 @@ namespace PetitSolune_PremierDamierAvecImages
         public MainWindow()
         {
             InitializeComponent();
+            Fonctions func = new Fonctions();
+            //définir la taille de la grille
+
+            for (int i = 0; i <= 3; i++)
+            {
+                ColumnDefinition colDef = new ColumnDefinition();
+                grdMain.ColumnDefinitions.Add(colDef);
+            }
+
+            //faire les boutons
+            Button btnEx1 = new Button();
+            btnEx1.Content = "Exercice 1";
+            Grid.SetColumn(btnEx1, 0);
+            Grid.SetRow(btnEx1, 0);
+            grdMain.Children.Add(btnEx1);
+
+            Button btnEx2 = new Button();
+            btnEx2.Content = "Exercice 2 (en création)";
+            Grid.SetColumn(btnEx2, 1);
+            Grid.SetRow(btnEx2, 0);
+            grdMain.Children.Add(btnEx2);
+
+            Button btnEx3 = new Button();
+            btnEx3.Content = "Exercice 3 (pas commencé)";
+            Grid.SetColumn(btnEx3, 2);
+            Grid.SetRow(btnEx3, 0);
+            grdMain.Children.Add(btnEx3);
+
+
+            btnEx1.Click += (s, e) =>
+            {
+                grdMain.Children.Clear();
+                grdMain.ColumnDefinitions.Clear();
+                grdMain.RowDefinitions.Clear();
+                func.Exercice1(grdMain);
+            };
+
+            btnEx2.Click += (s, e) =>
+            {
+                grdMain.Children.Clear();
+                grdMain.ColumnDefinitions.Clear();
+                grdMain.RowDefinitions.Clear();
+                func.Exercice2(grdMain);
+            };
+
+            btnEx3.Click += (s, e) =>
+            {
+                grdMain.Children.Clear();
+                grdMain.ColumnDefinitions.Clear();
+                grdMain.RowDefinitions.Clear();
+                func.Exercice3(grdMain);
+            };
+
+
+
         }
     }
 }
