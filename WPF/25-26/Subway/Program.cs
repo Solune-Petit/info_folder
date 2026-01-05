f8d38eb2c8c339db1a38d22c71f29dabf390f213
Solune Petit <210225@site.asty-moulin.be>
2026-01-05 09:29:36 +0100

    2026-01-05_09h29
    POO partie 2.1

---
+++ WPF/25-26/Subway/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Subway
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool anotherSandwich = false;
+            do
+            {
+                Console.WriteLine("Voulez vous un sandwich aléatoire");
+                string Uinput = Console.ReadLine().ToLower();
+                Console.Clear();
+                if (Uinput == "oui" || Uinput == "y")
+                {
+                    SandwichMaker sandwichMaker = new SandwichMaker();
+                    Console.WriteLine(sandwichMaker.composeSandwich());
+                    anotherSandwich = true;
+                }
+                else
+                {
+                    Console.WriteLine("pourquoi avoir lancé le programme alors ?");
+                    anotherSandwich = false;
+                }
+            } while (anotherSandwich);
+        }
+    }
+}
---
+++ WPF/25-26/Subway/SandwichMaker.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subway
+{
+    internal class SandwichMaker
+    {
+		private string[] _proteine;
+		private string[] _condiment;
+		private string[] _pain;
+		private string[] _crudite;
+
+		public string[] Crudite
+		{
+			get { return _crudite; }
+		}
+
+		public string[] Pain
+		{
+			get { return _pain; }
+		}
+
+		public string[] Proteine
+		{
+			get { return _proteine; }
+		}
+
+		public string[] Condiment
+		{
+			get { return _condiment; }
+		}
+
+		public SandwichMaker()
+		{
+			_proteine = new string[] { "Poulet", "Jambon", "Dinde", "Boeuf", "Tofu" };
+			_condiment = new string[] { "Mayo", "Ketchup", "Moutarde", "Sauce BBQ", "Vinaigrette" };
+			_pain = new string[] { "Blanc", "Complet", "Céréales", "Sans gluten", "Baguette" };
+			_crudite = new string[] { "Laitue", "Tomate", "Concombre", "Oignon", "Poivron" };
+        }
+
+
+		public string composeSandwich()
+		{
+			string[] sandwich = new string[4];
+			Random rand = new Random();
+			sandwich[0] = _pain[rand.Next(_pain.Length)];
+			sandwich[1] = _proteine[rand.Next(_proteine.Length)];
+			sandwich[2] = _crudite[rand.Next(_crudite.Length)];
+			sandwich[3] = _condiment[rand.Next(_condiment.Length)];
+			return $"Votre sandwich est composé de :" +
+				$"\n-	Pain : {sandwich[0]}" +
+				$"\n-	Protéine : {sandwich[1]}" +
+				$"\n-	Crudité : {sandwich[2]}" +
+				$"\n-	Condiment : {sandwich[3]}";
+        }
+
+
+    }
+}
