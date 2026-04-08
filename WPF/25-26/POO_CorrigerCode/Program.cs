938622d8c2a2789a6e2b2f0e2463f592a629541b
Solune Petit <210225@site.asty-moulin.be>
2026-04-08 09:03:31 +0200

    2026-04-08_09h03

---
+++ WPF/25-26/POO_CorrigerCode/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace POO_CorrigerCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random alea = new Random();
+            Console.WriteLine("Bienvenue dans ce petit programme de calcul mental.");
+            Calcul q = new Calcul((int)alea.Next(2) == 1);
+            while (true)
+            {
+                Console.Write($"{q.N1}{q.Op}{q.N2} = ");
+                string rep = Console.ReadLine();
+                int repEntiere;
+                // mauvaise entrée utilisateur
+                if (!int.TryParse(rep, out repEntiere))
+                {
+                    Console.WriteLine("Merci d'avoir joué");
+                    break;
+                }
+                // vérification de la réponse
+                else if (q.VerifOpe(repEntiere))
+                {
+                    Console.WriteLine("Correct !");
+                    q = new Calcul((int)alea.Next(2) == 1);
+                }
+                // mauvaise réponse au calcul
+                else
+                {
+                    Console.WriteLine("Erreur, recommencez !");
+                }
+            }
+        }
+
+    }
+}
